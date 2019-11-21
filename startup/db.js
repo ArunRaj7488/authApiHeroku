d2268c18dd35e03ae2190dfd1e4bdd5a844adf9b
Arun Gautam <arunraj1735169@gmail.com>
2019-11-22 02:10:07 +0530

    init commit

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
  
-module.exports = mongoose.connect('mongodb://localhost:27017/myAuth')
+module.exports = mongoose.connect('mongodb+srv://arun:arun@cluster0-wziy9.mongodb.net/test?retryWrites=true&w=majority')
         .then(()=>console.log('connected to mongodb'))
         .catch(err => console.log('Error: ',err));
 
